6d31dbe2e29f73264dbec1d5128044b8bd8e3a92
progrocker <mmiihhaa@list.ru>
2014-06-24 16:33:38 +0400

    commit

---
+++ momsite/js/scroll.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $('a[href*=#]').bind("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top-230
+      }, 700);
+      e.preventDefault();
+   });
+   return false;
+});
